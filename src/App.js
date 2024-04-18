8fd1cfc0f45d7166211daad9d98f87940384509e
Plasmaaa <147718245+Plasmaaa@users.noreply.github.com>
2024-04-18 14:43:22 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { useRef } from 'react';
 import './App.css';
 import footer from './Prouter.svg';
-import header from './header.svg';
+import logotitre from './logotitre.svg';
+import actu from './Icon Actu.svg'
+import compte from './Icon compte.svg'
+import pokedex from './Icon pokedex.svg'
+import quizz from './Icon quiz.svg'
+import scan from './Icon scan.svg'
 
 function App() {
   const videoRef = useRef(null);
@@ -26,11 +31,11 @@ function App() {
       context.drawImage(videoRef.current, 0, 0, 640, 480);
       const imageDataUrl = canvasRef.current.toDataURL('image/png');
 
-     
+
     }
   };
 
-  
+
   const onCircleClick = () => {
     startCamera();
     circleRef.current.addEventListener('click', captureImage);
@@ -39,7 +44,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={header} className="header" alt="header" />
+        <img src={logotitre} className="header" alt="header" />
         <div ref={circleRef} className="circle-container" onClick={onCircleClick}>
           <div className="circle">
             <div className="circle-text">
@@ -49,11 +54,20 @@ function App() {
         </div>
         <video ref={videoRef} className="video-feed" style={{ display: 'none' }} autoPlay></video>
         <canvas ref={canvasRef} className="image-canvas" style={{ display: 'none' }}></canvas>
+      
+
+      <body>
+        <div>
+          <img src={actu} className="icon1" alt="icone1" />
+          <img src={quizz} className="icon1" alt="icone2" />
+          <img src={scan} className="icon1" alt="icone3" />
+          <img src={pokedex} className="icon1" alt="icone4" />
+          <img src={compte} className="icon1" alt="icone5" />
+        </div>
+      </body>
       </header>
-      <footer>
-        <img src={footer} className='footer' alt="footer" />
-      </footer>
     </div>
+
   );
 }
 
