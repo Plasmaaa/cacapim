0edba2ec2da5580aefb4b07aae8e237d6f945054
Plasmaaa <147718245+Plasmaaa@users.noreply.github.com>
2024-04-18 11:01:29 +0200

    neuille

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const canvasRef = useRef(null);
   const circleRef = useRef(null);
 
-  // Fonction pour démarrer la caméra
+
   const startCamera = () => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
@@ -19,19 +19,18 @@ function App() {
     }
   };
 
-  // Fonction pour capturer l'image
+
   const captureImage = () => {
     if (videoRef.current && canvasRef.current) {
       const context = canvasRef.current.getContext('2d');
       context.drawImage(videoRef.current, 0, 0, 640, 480);
       const imageDataUrl = canvasRef.current.toDataURL('image/png');
 
-      // Ici, vous enverriez l'image au serveur pour la stocker dans votre BDD
-      // Utilisez l'API fetch ou axios pour envoyer l'image à votre serveur
+     
     }
   };
 
-  // Déclencheur pour démarrer la caméra et capturer l'image sur clic
+  
   const onCircleClick = () => {
     startCamera();
     circleRef.current.addEventListener('click', captureImage);
